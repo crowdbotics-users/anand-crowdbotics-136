37925647da8723b9c6795f816f34b3324242f69f
Saurav <sp.gharti@gmail.com>
2018-04-26 21:16:02 +0000

    add package django-request

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-request', 'url': 'http://pypi.python.org/pypi/django-request/1.5.4'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
     context = {
