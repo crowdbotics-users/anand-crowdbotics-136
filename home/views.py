71302a2047f4449f03131f551b8c6231bdbbac77
Saurav <sp.gharti@gmail.com>
2018-04-26 21:16:10 +0000

    add package django-rest-framework

--- home/views.py
+++ home/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 
 def home(request):
     packages = [
+	{'name':'django-rest-framework', 'url': 'http://pypi.python.org/pypi/django-rest-framework/0.1.0'},
 	{'name':'django-request', 'url': 'http://pypi.python.org/pypi/django-request/1.5.4'},
 	{'name':'django-allauth', 'url': 'http://pypi.python.org/pypi/django-allauth/0.34.0'},
     ]
